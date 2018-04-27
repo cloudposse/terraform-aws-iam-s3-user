c745c5958eb689ec193479c755f3c86cf5cd5f09
Andriy Knysh <aknysh@users.noreply.github.com>
2018-04-26 22:22:10 -0400

    Initial implementation (#1)
    * Initial implementation
    
    * Update `README`

---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "namespace" {
+  type        = "string"
+  description = "Namespace (e.g. `cp` or `cloudposse`)"
+}
+
+variable "stage" {
+  type        = "string"
+  description = "Stage (e.g. `prod`, `dev`, `staging`)"
+}
+
+variable "name" {
+  type        = "string"
+  description = "Application or solution name (e.g. `app`)"
+}
+
+variable "delimiter" {
+  type        = "string"
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
+}
+
+variable "attributes" {
+  type        = "list"
+  default     = []
+  description = "Additional attributes (e.g. `1`)"
+}
+
+variable "tags" {
+  type        = "map"
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "s3_actions" {
+  type        = "list"
+  default     = ["s3:GetObject"]
+  description = "Actions to allow in the policy"
+}
+
+variable "s3_resources" {
+  type        = "list"
+  description = "S3 resources to apply the actions specified in the policy"
+}
+
+variable "force_destroy" {
+  default     = "false"
+  description = "Destroy even if it has non-Terraform-managed IAM access keys, login profiles or MFA devices"
+}
+
+variable "path" {
+  default     = "/"
+  description = "Path in which to create the user"
+}
+
+variable "enabled" {
+  default     = "true"
+  description = "Set to false to prevent the module from creating any resources"
+}
