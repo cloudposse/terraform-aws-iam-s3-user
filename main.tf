e078005777eb058e83ae51077de2868ea602f8a5
aknysh <andriy.knysh@gmail.com>
2018-04-26 22:06:42 -0400

    Initial implementation

---
+++ main.tf
@@ -0,0 +1,20 @@
+data "aws_iam_policy_document" "default" {
+  statement {
+    actions   = ["${var.s3_actions}"]
+    resources = ["${var.s3_resources}"]
+    effect    = "Allow"
+  }
+}
+
+module "s3_user" {
+  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.2.2"
+  namespace     = "${var.namespace}"
+  stage         = "${var.stage}"
+  name          = "${var.name}"
+  attributes    = "${var.attributes}"
+  tags          = "${var.tags}"
+  enabled       = "${var.enabled}"
+  force_destroy = "${var.force_destroy}"
+  path          = "${var.path}"
+  policy        = "${data.aws_iam_policy_document.default.json}"
+}
