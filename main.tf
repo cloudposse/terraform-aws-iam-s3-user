5d147ea3dc9ef709fadc2f315ab8ab3d2b1adb33
Maxim Mironenko <mmironenko@chanzuckerberg.com>
2019-01-18 11:31:19 +0600

    aws_iam_policy_document data also respect 'enabled' flag (#7)
    * aws_iam_policy_document data also respect 'enabled' flag
    
    * lint check fix
    
    * using spat+join for resources with count

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 data "aws_iam_policy_document" "default" {
+  count = "${var.enabled == "true" ? 1 : 0}"
+
   statement {
     actions   = ["${var.s3_actions}"]
     resources = ["${var.s3_resources}"]
@@ -22,5 +24,5 @@ resource "aws_iam_user_policy" "default" {
   count  = "${var.enabled == "true" ? 1 : 0}"
   name   = "${module.s3_user.user_name}"
   user   = "${module.s3_user.user_name}"
-  policy = "${data.aws_iam_policy_document.default.json}"
+  policy = "${join("", data.aws_iam_policy_document.default.*.json)}"
 }
