4898f2e861ea3bd7ff8b15fe2bfc5763b9ac0e5e
Rohit Verma <rohit.verma@niki.ai>
2019-06-21 14:55:26 +0530

    upgraded-tf

--- main.tf
+++ main.tf
@@ -1,28 +1,29 @@
 data "aws_iam_policy_document" "default" {
-  count = "${var.enabled == "true" ? 1 : 0}"
+  count = var.enabled == "true" ? 1 : 0
 
   statement {
-    actions   = ["${var.s3_actions}"]
-    resources = ["${var.s3_resources}"]
+    actions   = var.s3_actions
+    resources = var.s3_resources
     effect    = "Allow"
   }
 }
 
 module "s3_user" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.4.0"
-  namespace     = "${var.namespace}"
-  stage         = "${var.stage}"
-  name          = "${var.name}"
-  attributes    = "${var.attributes}"
-  tags          = "${var.tags}"
-  enabled       = "${var.enabled}"
-  force_destroy = "${var.force_destroy}"
-  path          = "${var.path}"
+  source        = "git::https://github.com/rverma-nikiai/terraform-aws-iam-system-user.git?ref=master"
+  namespace     = var.namespace
+  stage         = var.stage
+  name          = var.name
+  attributes    = var.attributes
+  tags          = var.tags
+  enabled       = var.enabled
+  force_destroy = var.force_destroy
+  path          = var.path
 }
 
 resource "aws_iam_user_policy" "default" {
-  count  = "${var.enabled == "true" ? 1 : 0}"
-  name   = "${module.s3_user.user_name}"
-  user   = "${module.s3_user.user_name}"
-  policy = "${join("", data.aws_iam_policy_document.default.*.json)}"
+  count  = var.enabled == "true" ? 1 : 0
+  name   = module.s3_user.user_name
+  user   = module.s3_user.user_name
+  policy = join("", data.aws_iam_policy_document.default.*.json)
 }
+
--- variables.tf
+++ variables.tf
@@ -1,44 +1,44 @@
 variable "namespace" {
-  type        = "string"
+  type        = string
   description = "Namespace (e.g. `cp` or `cloudposse`)"
 }
 
 variable "stage" {
-  type        = "string"
+  type        = string
   description = "Stage (e.g. `prod`, `dev`, `staging`)"
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "Application or solution name (e.g. `app`)"
 }
 
 variable "delimiter" {
-  type        = "string"
+  type        = string
   default     = "-"
   description = "Delimiter to be used between `namespace`, `stage`, `name` and `attributes`"
 }
 
 variable "attributes" {
-  type        = "list"
+  type        = list(string)
   default     = []
   description = "Additional attributes (e.g. `1`)"
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(string)
   default     = {}
   description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "s3_actions" {
-  type        = "list"
+  type        = list(string)
   default     = ["s3:GetObject"]
   description = "Actions to allow in the policy"
 }
 
 variable "s3_resources" {
-  type        = "list"
+  type        = list(string)
   description = "S3 resources to apply the actions specified in the policy"
 }
 
@@ -56,3 +56,4 @@ variable "enabled" {
   default     = "true"
   description = "Set to false to prevent the module from creating any resources"
 }
+
