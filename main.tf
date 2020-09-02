ce0b17d76655f16e72058b1ef87bdda672413914
woz5999 <jeff.wozniak@umbralab.com>
2020-09-01 19:21:41 -0700

    pin minimum provider versions

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "default" {
 }
 
 module "s3_user" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.14.0"
+  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.16.0"
   namespace     = var.namespace
   stage         = var.stage
   environment   = var.environment
