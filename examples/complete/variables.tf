b3177599b8506d96d297da08da5108af4c685e0f
Iounes GARDON <me@iouns.me>
2020-09-18 19:22:51 +0200

    Update to context. (#22)
    * Update to context.
    
    * Remove useless label
    
    * Executed 'terraform fmt'
    
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -3,21 +3,6 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "name" {
-  type        = string
-  description = "The Name of the application or solution  (e.g. `bastion` or `portal`)"
-}
-
-variable "namespace" {
-  type        = string
-  description = "Namespace, which could be your organization name or abbreviation, e.g. 'eg' or 'cp'"
-}
-
-variable "stage" {
-  type        = string
-  description = "Stage (e.g. `prod`, `dev`, `staging`)"
-}
-
 variable "s3_actions" {
   type        = list(string)
   description = "Actions to allow in the policy"
--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "aws_iam_policy_document" "default" {
-  count = var.enabled ? 1 : 0
+  count = module.this.enabled ? 1 : 0
 
   statement {
     actions   = var.s3_actions
@@ -10,19 +10,14 @@ data "aws_iam_policy_document" "default" {
 
 module "s3_user" {
   source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.16.0"
-  namespace     = var.namespace
-  stage         = var.stage
-  environment   = var.environment
-  name          = var.name
-  attributes    = var.attributes
-  tags          = var.tags
-  enabled       = var.enabled
   force_destroy = var.force_destroy
   path          = var.path
+
+  context = module.this.context
 }
 
 resource "aws_iam_user_policy" "default" {
-  count  = var.enabled ? 1 : 0
+  count  = module.this.enabled ? 1 : 0
   name   = module.s3_user.user_name
   user   = module.s3_user.user_name
   policy = join("", data.aws_iam_policy_document.default.*.json)
