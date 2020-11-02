5aa82c569b4861db804149e62b279a5dfb873191
Jurgen Weber <jurgen@space-marine.org>
2020-11-02 11:51:18 +1100

    remove top end version constraints

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "default" {
 }
 
 module "s3_user" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.16.0"
+  source        = "git::https://github.com/jurgenweber/terraform-aws-iam-system-user.git?ref=master"
   namespace     = var.namespace
   stage         = var.stage
   environment   = var.environment
