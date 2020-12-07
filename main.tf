7a4a2667b844d46f4fc0eb39904c5bf3e3b7c7fd
Jürgen Weber <jurgen@space-marine.org>
2020-12-07 12:33:49 +1100

    update for v14 compat

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "aws_iam_policy_document" "default" {
 }
 
 module "s3_user" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=v0.17.0"
+  source        = "git::https://github.com/cloudposse/terraform-aws-iam-system-user.git?ref=tags/0.17.0"
   namespace     = var.namespace
   stage         = var.stage
   environment   = var.environment
